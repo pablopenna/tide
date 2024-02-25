75d0e6c7b7ce21c236bd797ec965e02268f61a01
Pablo <pablo_penna7@hotmail.com>
2024-02-25 16:38:12 +0000

    Add troop scene that follows the player

---
+++ scenes/troop/troop.gd
@@ -0,0 +1,29 @@
+extends Entity
+
+@export var entity_to_follow: Entity
+var target_position_offset: Vector2
+const TARGET_POSITION_ACCEPTED_DISTANCE: float = 1
+@export var speed: float = 50
+
+func ready():
+	# TODO: make it auto by a factory
+	target_position_offset = Vector2(10, 10)
+	
+func process(_delta):
+	if _should_move_to_target_position():
+		velocity = _get_target_direction() * speed
+	else:
+		velocity = Vector2.ZERO
+
+func _get_target_position() -> Vector2:
+	return entity_to_follow.position + target_position_offset
+
+func _get_target_direction() -> Vector2:
+	return position.direction_to(_get_target_position())
+
+func _get_distance_to_target_position() -> float:
+	var target_position = _get_target_position()
+	return position.distance_to(target_position)
+
+func _should_move_to_target_position() -> bool:
+	return _get_distance_to_target_position() >= TARGET_POSITION_ACCEPTED_DISTANCE
